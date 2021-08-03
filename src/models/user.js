d577b96306c0f62993ef6d0ddd3d3ea676a08dad
Rahul Kumar <rahul080924@outlook.com>
2021-08-03 13:01:59 +0530

    validation added for user

--- src/models/user.js
+++ src/models/user.js
@@ -8,12 +8,21 @@ const User = new Schema({
 	},
 	email: {
 		type: String,
+		validate: {
+			validator: function (rawEmail) {
+				const re =
+					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+				return re.test(String(rawEmail).toLowerCase());
+			},
+			message: (props) => `${props.value} is not a valid email address!`,
+		},
 		required: true,
 	},
 	phone: {
 		type: String,
 		minLength: 10,
 		maxLength: 10,
+		required: true,
 	},
 	password: {
 		type: String,
@@ -23,6 +32,10 @@ const User = new Schema({
 	role: {
 		type: String,
 		required: true,
+		enum: {
+			values: ['admin', 'customer'],
+			message: '{VALUE} is not supported',
+		},
 	},
 	profilePic: {
 		type: String,
@@ -34,4 +47,4 @@ const User = new Schema({
 	},
 });
 
-module.exports = Users = mongoose.model('User', User);
+module.exports = mongoose.model('User', User);
