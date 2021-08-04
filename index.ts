d29060dbddc4466918d65da26202eb674b915a38
Rahul Kumar <rahul080924@outlook.com>
2021-08-04 17:13:59 +0530

    change in port

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const keys = config({ path: './src/config/.env' });
 import { connect, Mongoose } from 'mongoose';
 
 //Initialization of port number
-const port: number | string = Number(process.env.PORT) || 8000;
+const port: number | string = process.env.PORT || 8000;
 
 // importing all the routes
 import { authRouter } from './src/api/auth';
